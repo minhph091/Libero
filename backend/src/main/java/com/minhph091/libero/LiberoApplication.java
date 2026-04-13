55149eefd0b3ed54828a3e5b5e1c57315c1ec4c8
Mei <137881837+minhotaku@users.noreply.github.com>
2026-04-13 15:00:23 +0700

    feat: khởi tạo thư mục dự án, thêm các dependency cần thiết

---
+++ backend/src/main/java/com/minhph091/libero/LiberoApplication.java
@@ -0,0 +1,13 @@
+package com.minhph091.libero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiberoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiberoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/minhph091/libero/LiberoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhph091.libero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiberoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
